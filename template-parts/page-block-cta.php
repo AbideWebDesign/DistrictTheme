7cb786b209c6673b824a8184186f42106133b7bb
Josh Armentano <josh@abidewebdesign.com>
2018-10-23 13:18:13 -0700

    Update: CTA block.

--- template-parts/page-block-cta.php
+++ template-parts/page-block-cta.php
@@ -5,18 +5,20 @@
 	<div class="row">
 		<?php if($type == "Form"): ?>
 			<div id="cta-block-heading" class="<?php echo (get_sub_field('call_to_action_form_type') == 'Horizontal' ? 'col-lg-4 text-right' : 'col-xs-12 margin-bottom-half'); ?>">
-				<?php the_sub_field('call_to_action_heading'); ?>
+				<h2><?php the_sub_field('call_to_action_heading'); ?></h2>
 			</div>
 			<div class="<?php echo (get_sub_field('call_to_action_form_type') == 'Horizontal' ? 'col-lg-8' : 'col-xs-12'); ?>">
 				<?php $form_object = get_sub_field('call_to_action_form'); ?>
 				<?php echo do_shortcode('[gravityform id="' . $form_object['id'] . '" title="false" description="false" ajax="true"]'); ?>
 			</div>
 		<?php elseif ($type == "Button"): ?>
-				<div id="cta-block-heading" class="<?php echo (get_sub_field('call_to_action_button_type') == 'Horizontal' ? 'col-lg-9 text-right' : 'col-xs-12 margin-bottom-half text-center'); ?>">
-					<?php the_sub_field('call_to_action_heading'); ?>
-				</div>
-				<div id="cta-block-button" class="<?php echo (get_sub_field('call_to_action_button_type') == 'Horizontal' ? 'col-lg-3' : 'col-xs-12 text-center'); ?>">
-					<a href="<?php the_sub_field('call_to_action_button_link'); ?>" class="btn btn-white"><?php the_sub_field('call_to_action_button_label'); ?></a>
+				<div id="cta-block-button" class="<?php echo (get_sub_field('call_to_action_button_type') == 'Horizontal' ? 'col-lg-12' : 'col-xs-12 margin-bottom-half text-center'); ?>">
+					<ul class="list-unstyled <?php echo (get_sub_field('call_to_action_button_type') == 'Horizontal' ? 'list-inline' : ''); ?>">
+						<li><h2 class="<?php echo (get_sub_field('call_to_action_button_type') == 'Vertical' ? 'margin-bottom-half' : 'margin-bottom-zero'); ?>"><?php the_sub_field('call_to_action_heading'); ?></h2></li>
+						<li id="cta-block-button">
+							<a href="<?php the_sub_field('call_to_action_button_link'); ?>" class="btn btn-white"><?php the_sub_field('call_to_action_button_label'); ?></a>
+						</li>
+					</ul>
 				</div>
 		<?php endif; ?>
 	</div>

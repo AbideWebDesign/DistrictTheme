c64a370389e1cc32c0db1cc31c900ac9c99cdda3
Josh Armentano <josh@abidewebdesign.com>
2018-10-23 14:00:56 -0700

    Fix: Bug with display on horizontal orientation for CTA block.

--- template-parts/page-block-cta.php
+++ template-parts/page-block-cta.php
@@ -14,10 +14,8 @@
 		<?php elseif ($type == "Button"): ?>
 				<div id="cta-block-button" class="<?php echo (get_sub_field('call_to_action_button_type') == 'Horizontal' ? 'col-lg-12' : 'col-xs-12 margin-bottom-half text-center'); ?>">
 					<ul class="list-unstyled <?php echo (get_sub_field('call_to_action_button_type') == 'Horizontal' ? 'list-inline' : ''); ?>">
-						<li><h2 class="<?php echo (get_sub_field('call_to_action_button_type') == 'Vertical' ? 'margin-bottom-half' : 'margin-bottom-zero'); ?>"><?php the_sub_field('call_to_action_heading'); ?></h2></li>
-						<li id="cta-block-button">
-							<a href="<?php the_sub_field('call_to_action_button_link'); ?>" class="btn btn-white"><?php the_sub_field('call_to_action_button_label'); ?></a>
-						</li>
+						<li><h2 class="<?php echo (get_sub_field('call_to_action_button_type') == 'Vertical' ? 'margin-bottom-half' : 'margin-bottom-none'); ?>"><?php the_sub_field('call_to_action_heading'); ?></h2></li>
+						<li><a href="<?php the_sub_field('call_to_action_button_link'); ?>" class="btn btn-white"><?php the_sub_field('call_to_action_button_label'); ?></a></li>
 					</ul>
 				</div>
 		<?php endif; ?>

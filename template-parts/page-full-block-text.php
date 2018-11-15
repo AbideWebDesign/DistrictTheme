ee8f1ea77f237fa861b24dbe40aea5636dbafe6f
Josh Armentano <josh@abidewebdesign.com>
2018-11-15 13:34:10 -0800

    Added: Optional image for text block on full width page builder.

--- template-parts/page-full-block-text.php
+++ template-parts/page-full-block-text.php
@@ -3,14 +3,17 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="text-block-wrap" <?php echo (get_sub_field('text_block_image_side') == 'Right' ? 'style="flex-direction: row-reverse;"' : ''); ?>>
-					<div class="text-block-image">
-						<?php $image = get_sub_field('text_block_image'); ?>
-						<?php echo wp_get_attachment_image($image, 'Text Block', false); ?>
-					</div>
+					<?php if(get_sub_field('include_text_block_image')): ?>
+						<div class="text-block-image">
+							<?php $image = get_sub_field('text_block_image'); ?>
+							<?php echo wp_get_attachment_image($image, 'Text Block', false); ?>
+						</div>
+					<?php endif; ?>
 					<div class="text-block-content">
 						<h2 class="margin-bottom-one"><?php the_sub_field('text_block_heading'); ?></h2>
 						<?php the_sub_field('text_block_content'); ?>
 					</div>
+					
 				</div>
 			</div>
 		</div>

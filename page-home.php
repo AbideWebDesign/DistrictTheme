b080f5b3fd11bba4a76763d325a728935ddb1ebd
Josh Armentano <josh@abidewebdesign.com>
2018-12-20 13:19:03 -0800

    Update: Added back left and right arrows to carousel.

--- page-home.php
+++ page-home.php
@@ -62,7 +62,14 @@ get_header(); ?>
 					endforeach; ?>
 					
 				</div>
-								
+				<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>				
 			<?php endif; ?>
 		</div>
 	</section>

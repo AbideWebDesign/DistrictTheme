79ba8082e2b2a2e3446d3cb5e1b4f33065a60d05
Josh Armentano <josh@abidewebdesign.com>
2021-11-03 12:12:22 -0700

    no message

--- template-parts/page-full-block-notification.php
+++ template-parts/page-full-block-notification.php
@@ -1,4 +1,4 @@
-<div class="py-2">
+<div class="pt-1 pb-2">
 
 	<div class="container">
 
@@ -8,9 +8,9 @@
 
 				<div class="notification d-flex flex-column flex-sm-row bg-secondary shadow-sm rounded py-1 px-1 px-lg-2 w-100 align-items-center text-center">
 
-					<div class="mb-1 mb-md-0"><i class="fa fa-bullhorn text-white mr-1"></i> <?php the_sub_field('notification_text'); ?></div>
+					<div class="mb-1 mb-md-0 text-lg"><i class="fa fa-bullhorn text-white mr-1"></i> <?php the_sub_field('notification_text'); ?></div>
 
-					<?php if ( get_sub_field('notification_button_link') ): ?>
+					<?php if ( get_sub_field('notification_include_link') ): ?>
 					
 						<div class="ml-md-auto"><a class="btn btn-light" href="<?php the_sub_field('notification_button_link'); ?>"><?php the_sub_field('notification_button_label'); ?></a></div>
 						

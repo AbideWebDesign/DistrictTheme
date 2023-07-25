be77557be38602909f79dc7886f7d8762d0da49c
Josh Armentano <josh@abidewebdesign.com>
2023-07-25 13:58:08 -0700

    Updates to footer

--- footer.php
+++ footer.php
@@ -135,9 +135,7 @@
 							</div>
 							<div class="col-lg-5">
 								<div class="text-xs text-white">
-									<p class="mb-0"><?php the_field('statement', 'options'); ?></p>
-									<p class="mb-0"><?php the_field('statement_contact_name', 'options'); ?></p>
-									<div><?php the_field('statement_contact_title', 'options'); ?></div>
+									<div class="footer-text"><?php the_field('non_discrimination_statement', 'options'); ?></div>
 									<p class="mb-0"><a href="mailto:<?php the_field('statement_contact_email', 'options'); ?>" target="_blank"><?php the_field('statement_contact_email', 'options'); ?></a> <?php the_field('statement_contact_phone', 'options'); ?></p>
 								</div>
 							</div>

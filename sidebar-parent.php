6ec77b4131a4e9110aa659da154af8ce1d4d8223
Josh Armentano <josh@abidewebdesign.com>
2024-09-17 10:43:45 -0700

    no message

--- sidebar-parent.php
+++ sidebar-parent.php
@@ -7,9 +7,6 @@
  * @since CSD 1.0
  */
 ?>
-
-<?php if ( is_active_sidebar( 'sidebar' )  ) : ?>
-	<aside id="secondary" class="sidebar widget-area" role="complementary">
-		<?php get_template_part('template-parts/content','sidenav'); ?>
-	</aside><!-- .sidebar .widget-area -->
-<?php endif; ?>
+<aside id="secondary" class="sidebar widget-area" role="complementary">
+	<?php get_template_part('template-parts/content','sidenav'); ?>
+</aside><!-- .sidebar .widget-area -->
